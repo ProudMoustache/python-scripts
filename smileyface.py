818478fbd724afcd1ab5b6bbbe851f9f91e7c2c4
ProudMoustache <henrysamhillman@gmail.com>
2015-01-23 17:25:47 -0500

    Create smileyface.py

---
+++ smileyface.py
@@ -0,0 +1,39 @@
+from turtle import*
+
+
+
+fill(True)
+color("Yellow")
+
+for i in range(90):
+    pensize(1)
+    pencolor("yellow")
+    right(4)
+    forward(5)
+    hideturtle()
+
+fill(False)
+
+#lol.hideturtle()
+penup()
+pencolor("black")
+right(90)
+forward(60)
+pendown()
+for i in range(20):
+    forward(1)
+    left(5)
+    right(3)
+for i in range(30):
+    forward(1)
+    right(4)
+penup()
+
+
+    
+
+done()
+
+
+
+
